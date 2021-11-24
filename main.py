82077c792a425402fc5bed0cef395aa07aa2a85a
Will-HH <whuaman@unitru.edu.pe>
2021-11-23 20:12:42 -0500

    Editando el archivo main

--- main.py
+++ main.py
@@ -9,4 +9,17 @@ from PySide6.QtWidgets import *
 
 #import UIS
 from ui_main import Ui_MainWindow
-from ui_main import
+from ui_splash_screen import Ui_SplashScreen
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.ui=Ui_MainWindow()
+        self.ui.setupUi(self)
+        
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window=MainWindow()
+    sys.exit(app.exec())
