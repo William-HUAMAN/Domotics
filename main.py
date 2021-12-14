8b28d405bffba6e846a9bcb8bbb41287addae08b
Will-HH <whuaman@unitru.edu.pe>
2021-12-14 16:33:20 -0500

    editing main

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print('hola')
+# ///////////////////////////////////////////////////////////
+#
+# BY:WILLIAM HUAMÁN HUAMÁN
+# PROJECT: E-THAN
+# V: 1.0.0
+#
+# ///////////////////////////////////////////////////////////
+
+# IMPORT MODULES
+import sys
+import os
+
+# IMPORT QT CORE
+from qt_core import *
+
+# IMPORT MAIN WINDOW
+from gui.windows.main_window import *
+
+
+# MAIN WINDOW
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec())
