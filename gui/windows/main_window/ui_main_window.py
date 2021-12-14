6f10282ab4bbfe53500dc7c2f28d4d6c26545483
Will-HH <whuaman@unitru.edu.pe>
2021-12-14 18:09:54 -0500

    Defining background color  2

--- gui/windows/main_window/ui_main_window.py
+++ gui/windows/main_window/ui_main_window.py
@@ -14,6 +14,9 @@ from qt_core import *
 class UI_MainWindow(object):
 
     def __init__(self):
+        self.content = None
+        self.left_menu = None
+        self.main_layout = None
         self.central_frame = None
 
     def setup_ui(self, parent):
@@ -25,8 +28,19 @@ class UI_MainWindow(object):
         parent.setMinimumSize(960, 520)
 
         # CREATE CENTRAL WIDGET
-        self.central_frame = QFrame(None)
+        self.central_frame = QFrame()
         self.central_frame.setStyleSheet("background-color: #111315")
+
+        # CREATE MAIN LAYOUT
+        self.main_layout = QHBoxLayout(self.central_frame)
+
+        # CREATE LEFT MENU
+        self.left_menu = QFrame()
+        self.left_menu.setStyleSheet("background-color: #44475a")
+
+        # CONTENT
+        self.content = QFrame()
+        self.content.setStyleSheet("background-color: #282a36")
 
         # SET CENTRAL WIDGET
         parent.setCentralWidget(self.central_frame)
