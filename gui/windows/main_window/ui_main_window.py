6b563f5f1b4668be15e7587ed98007604d359ef4
Will-HH <whuaman@unitru.edu.pe>
2021-12-14 17:47:11 -0500

    Defining background color

--- gui/windows/main_window/ui_main_window.py
+++ gui/windows/main_window/ui_main_window.py
@@ -5,3 +5,28 @@
 # V: 1.0.0
 #
 # ///////////////////////////////////////////////////////////
+
+# IMPORT CORE
+from qt_core import *
+
+
+# MAIN WINDOW
+class UI_MainWindow(object):
+
+    def __init__(self):
+        self.central_frame = None
+
+    def setup_ui(self, parent):
+        if not parent.objectName():
+            parent.setObjectName("MainWindow")
+
+        # SET INITIAL PARAMETERS
+        parent.resize(1000, 500)
+        parent.setMinimumSize(960, 520)
+
+        # CREATE CENTRAL WIDGET
+        self.central_frame = QFrame(None)
+        self.central_frame.setStyleSheet("background-color: #111315")
+
+        # SET CENTRAL WIDGET
+        parent.setCentralWidget(self.central_frame)
--- main.py
+++ main.py
@@ -14,13 +14,17 @@ import os
 from qt_core import *
 
 # IMPORT MAIN WINDOW
-from gui.windows.main_window import *
+from gui.windows.main_window.ui_main_window import UI_MainWindow
 
 
 # MAIN WINDOW
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        # SETUP MAIN WINDOW
+        self.ui = UI_MainWindow
+        self.ui.setup_ui(self, self)
 
         self.show()
 
